fixed masking and MultiHeadAttention

layers.py
@@ -48,18 +48,19 @@ class MultiHeadAttention(nn.Module):
     def forward(self, query, key, value):
         head_attns = []
         for i in range(self.n_heads):
-            query_proj = query * self.WQ[i]
-            key_proj = key * self.WK[i]
-            value_proj = value * self.WV[i]
+            query_proj = self.WQ[i](query)
+            key_proj = self.WK[i](key)
+            value_proj = self.WV[i](value)
             attn = self.scaled_dot_product_attention(query_proj, key_proj, value_proj)
             head_attns.append(attn)
-        return torch.cat(head_attns, dim=1) * self.WO
+        return self.WO(torch.cat(head_attns, dim=1))
 
     def scaled_dot_product_attention(self, query, key, value):
         d_k = query.size(-1)
         query_key = torch.matmul(query, key.transpose(-2, -1))
         if self.masked:
-            query_key = torch.tril(query_key)
+            mask = torch.tril(torch.ones(query_key.size())).type(torch.uint8)
+            query_key.masked_fill_(mask == 0, -1e9)
         scores = log_softmax(query_key / math.sqrt(d_k), dim=0)
         return torch.matmul(scores, value)
 

main.py
@@ -6,8 +6,6 @@ from layers import Embed, EncoderStack, DecoderStack
 
 
 
-
-
 src_vocab_size = 1000
 tgt_vocab_size = 800
 d_vocab = src_vocab_size + tgt_vocab_size
@@ -18,20 +16,21 @@ d_ff = 2048
 d_k = 64
 d_v = 64
 
+
 X = torch.randint(src_vocab_size, (n, 1)).squeeze()
 Y = torch.randint(tgt_vocab_size, (m, 1)).squeeze()
 
-X_token = one_hot(X, num_classes=d_vocab)
-Y_token = one_hot(Y, num_classes=d_vocab)
+X_token = one_hot(X, num_classes=d_vocab).type(torch.float)
+Y_token = one_hot(Y, num_classes=d_vocab).type(torch.float)
 
 
 embed_layer = Embed(d_vocab, d_mod)
 
 encoder = EncoderStack(embed_layer, d_vocab, n, d_mod, d_ff, d_k, d_v)
-memory = encoder(X)
+memory = encoder(X_token)
 
 decoder = DecoderStack(embed_layer, memory, d_vocab, m, d_mod, d_ff, d_k, d_v)
-prob_dist = decoder(Y)
+prob_dist = decoder(Y_token)
 
 print(prob_dist.size())
 sum_check = torch.sum(torch.exp(prob_dist), dim=1)